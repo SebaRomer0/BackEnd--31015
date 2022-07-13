1c51930a1110f57a9f56f9547237a9e4396f319f
Romero Sebastian <sebastian.homero@outlook.com>
2022-07-13 18:55:35 -0300

    Subiendo pruebas de desafios

---
+++ Desafio-3-Servidor/test/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+const PORT = 8080
+
+app.get('/', (req,res) => {
+    res.send({mensaje: 'Sulute a Pepe'})
+})
+
+// cambiando / indicas otra ruta y aprece otro mensaje
+app.get('/pepe', (req,res) => {
+    res.send({mensaje: 'Sos los mas Seba'})
+})
+
+
+// La conexcion server con el puerto
+const server = app.listen(PORT, () => {
+    console.log(`Server listening [${PORT}]...`);
+})
