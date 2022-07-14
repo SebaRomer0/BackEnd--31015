f5ea3ac259943256fde1552429de3112fff52cbf
Romero Sebastian <sebastian.homero@outlook.com>
2022-07-13 21:45:45 -0300

    Desafio servidores terminado- Express

--- Desafio-3-Servidor/test/index.js
+++ Desafio-3-Servidor/test/index.js
@@ -1,9 +1,29 @@
-const http = require('http')
+const express = require('express')
 
-const server = http.createServer( (peticion, respuesta) => {
-    respuesta.end('Vamos Sebita que nos estamos poniendo al dia')
-} )
+const app = express()
+const PORT = 8080
 
-const connectedServer = server.listen(8080, ()=> {
-    console.log(`Server runnig...`);
+const Productos = require('./productos.json')
+
+// Pagina principal
+app.get('/', (req,res) => {
+    res.send("<h1>Desafio de Servidor Express</h1> ")
+})
+
+// GET Productos
+app.get('/producto', (req,res) => {
+    res.send(`<h2>Todos los productos Cargados ,[${JSON.stringify(Productos)}]</h2> `)
+})
+
+
+// GET Productos Random
+app.get('/random', (req,res) => {
+    let random = Productos[Math.floor(Math.random() * Productos.length)]
+    res.send(`<h2>Esto son uno de los productos Cargados</h2><br>,${JSON.stringify(random)}`)
+})
+
+
+// Conexion con el servidor 
+const server = app.listen(PORT, () => {
+    console.log(`Server funcionando [${PORT}]`);
 })
